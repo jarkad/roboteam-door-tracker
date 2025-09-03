ee2bb925a45337520132efb584ebdce3d673071b
Night-Swan <184144226+Night-Swan@users.noreply.github.com>
2025-09-03 12:00:36 +0200

    Allow admin to make changes to db

--- door_tracker/webui/admin.py
+++ door_tracker/webui/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SubTeam, Person, TagPerson, Membership, Log
+
+admin.site.register([SubTeam, Person, TagPerson, Membership, Log])
+
+
