67a8e8ab871722478a866f364fea8205c19b9a9c
jarkad <jarkad@jarkad.net.eu.org>
2025-09-03 13:11:21 +0200

    model: add the "forever data" table

--- door_tracker/webui/admin.py
+++ door_tracker/webui/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
+from django.contrib.admin.views.main import ChangeList
 
-from .models import SubTeam, Person, TagPerson, Log
+from .models import Log, Membership, Tag, Person, SubTeam, Job
 
-admin.site.register([SubTeam, Person, TagPerson, Log])
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'type', 'person')
+
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ('starting_from', 'person', 'subteam', 'job')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'binary_id')
+
+
+@admin.register(Person, SubTeam)
+class NamedAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quota')
--- door_tracker/webui/models.py
+++ door_tracker/webui/models.py
@@ -1,26 +1,72 @@
+from base64 import b64encode
 from django.db import models
-
-
-class SubTeam(models.Model):
-    name = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-
-class Person(models.Model):
-    name = models.TextField()
-    subteam_id = models.ManyToManyField(SubTeam)
-
-    def __str__(self):
-        return self.name
-
-
-class TagPerson(models.Model):
-    person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
+from django.utils import timezone
 
 
 class Log(models.Model):
-    type = models.TextField()
-    tag_id = models.ForeignKey(TagPerson, on_delete=models.CASCADE)
+    class LogEntryType(models.TextChoices):
+        CHECKIN = 'IN', 'Check-in'
+        CHECKOUT = 'OUT', 'Check-out'
+        UNKNOWN = 'WTF', 'Card not linked'
+
+    type = models.CharField(max_length=3, choices=LogEntryType.choices)
+    tag = models.ForeignKey('Tag', on_delete=models.CASCADE, related_name='logs')
     time = models.DateTimeField(auto_now_add=True)
+
+    def timestamp(self):
+        return self.time.strftime('%c')
+
+    def person(self):
+        return f'{self.tag.owner} ({self.tag.name})'
+
+    def __str__(self):
+        return ' — '.join([self.timestamp(), self.type, self.person()])
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(
+        'Person', on_delete=models.CASCADE, related_name='memberships'
+    )
+    subteam = models.ForeignKey(
+        'SubTeam',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='members',
+    )
+    job = models.ForeignKey('Job', blank=True, null=True, on_delete=models.CASCADE)
+    starting_from = models.DateTimeField(default=timezone.now)
+
+
+class Tag(models.Model):
+    tag = models.BinaryField()
+    name = models.CharField()
+    owner = models.ForeignKey('Person', on_delete=models.CASCADE, related_name='tags')
+
+    def binary_id(self):
+        return b64encode(self.tag).decode()
+
+    def __str__(self):
+        return f'{self.name} ({self.owner})'
+
+
+class Person(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+
+class SubTeam(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    name = models.CharField()
+    quota = models.IntegerField(verbose_name='Quota (hours per week)')
+
+    def __str__(self):
+        return f'{self.name} ({self.quota} hrs/week)'
