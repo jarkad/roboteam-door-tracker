aebbaf8a134d706243719a264fcd39a3687fb39f
jarkad <jarkad@jarkad.net.eu.org>
2025-09-07 17:26:32 +0200

    git: run pre-commit on all files

--- door_tracker/webui/urls.py
+++ door_tracker/webui/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.new_login, name="login"),
-    path('logout', views.new_logout, name="logout"),
+    path('login', views.new_login, name='login'),
+    path('logout', views.new_logout, name='logout'),
 ]
-
-
--- door_tracker/webui/views.py
+++ door_tracker/webui/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
-from .models import Log, Tag
 from django.http import JsonResponse
 
 # Create your views here.
@@ -16,7 +14,7 @@ def index(request):
 def new_login(request):
     if request.user.is_authenticated:
         return redirect('index')
-    
+
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
@@ -25,15 +23,25 @@ def new_login(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return JsonResponse({'status': 'success', 'message': f'Welcome Comrade {username}'}, status=200)
+                return JsonResponse(
+                    {'status': 'success', 'message': f'Welcome Comrade {username}'},
+                    status=200,
+                )
             else:
-                return JsonResponse({'status': 'error', 'message': 'Invalid username or password'}, status=401)
+                return JsonResponse(
+                    {'status': 'error', 'message': 'Invalid username or password'},
+                    status=401,
+                )
         else:
-            return JsonResponse({'status': 'error', 'message': 'Invalid username or password'}, status=400)
-    
+            return JsonResponse(
+                {'status': 'error', 'message': 'Invalid username or password'},
+                status=400,
+            )
+
     return render(request, 'webui/login.html')
+
 
 def new_logout(request):
     logout(request)
-    messages.success(request, "Logged out")
+    messages.success(request, 'Logged out')
     return redirect('login')
