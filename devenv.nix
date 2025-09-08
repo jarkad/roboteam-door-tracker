52974a88830a1fb9b78dd06e4f31a32113ca746b
jarkad <jarkad@jarkad.net.eu.org>
2025-09-08 17:42:57 +0200

    devcontainer: make `dev` script default and autostart it on folder open

--- devenv.nix
+++ devenv.nix
@@ -137,12 +137,20 @@ in
     version = "2.0.0";
     tasks =
       # scripts
-      lib.mapAttrsToList (name: _: {
-        label = "script: ${name}";
-        type = "shell";
-        command = name;
-        group = "build";
-      }) (lib.removeAttrs config.scripts [ "django" ])
+      lib.mapAttrsToList (
+        name: _:
+        {
+          label = "script: ${name}";
+          type = "shell";
+          command = name;
+          group = "build";
+        }
+        // lib.optionalAttrs (name == "dev") {
+          group.kind = "build";
+          group.isDefault = name == "dev";
+          runOptions.runOn = "folderOpen";
+        }
+      ) (lib.removeAttrs config.scripts [ "django" ])
       # containers
       ++ lib.mapAttrsToList (name: _: {
         label = "container: ${name}";
