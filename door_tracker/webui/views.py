087ba06c3d3293623979a099f868d379446af41d
Andrei Badea <badea.andrei1908@gmail.com>
2025-09-08 16:42:56 +0000

    testing pushing

--- door_tracker/webui/views.py
+++ door_tracker/webui/views.py
@@ -10,6 +10,7 @@ import datetime
 from .models import Log
 from .models import Tag
 
+
 def index(request):
     if not request.user.is_authenticated:
         return redirect('login')
@@ -51,41 +52,53 @@ def new_logout(request):
     messages.success(request, 'Logged out')
     return redirect('login')
 
+
 def check_status(request):
     if not request.user.is_authenticated:
         return JsonResponse(
-                    {'status': 'error', 'message': 'Error. Log in to view data.'},
-                    status=400,
-                )
-    last_log = Log.objects.filter(tag__owner=request.user).select_related("tag").order_by("-time").first()
+            {'status': 'error', 'message': 'Error. Log in to view data.'},
+            status=400,
+        )
+    last_log = (
+        Log.objects.filter(tag__owner=request.user)
+        .select_related('tag')
+        .order_by('-time')
+        .first()
+    )
     if not last_log:
         return JsonResponse(
             {'status': 'error', 'message': 'Error. Log not found.'},
-             status=400,
+            status=400,
         )
-    return JsonResponse({'status': last_log.get_type_display(), 'date': last_log.time.isoformat()}, status=200)
+    return JsonResponse(
+        {'status': last_log.get_type_display(), 'date': last_log.time.isoformat()},
+        status=200,
+    )
 
-    
-def change_status (request):
+
+def change_status(request):
     last_status = check_status(request)
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
         except json.JSONDecodeError:
             return JsonResponse(
-                    {'status': 'error', 'message': 'Error. Invalid json data.'},
-                    status=400,
-                )
-        
-        tag_id = data["tag_id"]
-        tag_scanned  = Tag.objects.filter(id = tag_id).first()
-        owner = tag_scanned.owner
+                {'status': 'error', 'message': 'Error. Invalid json data.'},
+                status=400,
+            )
+
+        tag_id = data['tag_id']
+        tag_scanned = Tag.objects.filter(id=tag_id).first()
+        # owner = tag_scanned.owner
         new_status = Log.LogEntryType.CHECKOUT
 
-        if (last_status.status==Log.LogEntryType.CHECKOUT):
+        if last_status.status == Log.LogEntryType.CHECKOUT:
             new_status = Log.LogEntryType.CHECKIN
 
-        log = Log.objects.create(type=new_status, Tag=tag_scanned, time=datetime.datetime.now().isoformat())
+        log = Log.objects.create(
+            type=new_status, Tag=tag_scanned, time=datetime.datetime.now().isoformat()
+        )
+        print('XDD')
         log.save(force_insert=True)
     return
 
@@ -93,21 +106,26 @@ def change_status (request):
 def current_user_data(request):
     if not request.user.is_authenticated:
         return JsonResponse(
-                    {'status': 'error', 'message': 'Error. Log in to view data.'},
-                    status=400,
-                )
-    
-    logs = Log.objects.filter(tag__owner=request.user).select_related("tag").order_by("-time")
+            {'status': 'error', 'message': 'Error. Log in to view data.'},
+            status=400,
+        )
+
+    logs = (
+        Log.objects.filter(tag__owner=request.user)
+        .select_related('tag')
+        .order_by('-time')
+    )
 
     data = []
     for log in logs:
-        data.append({
-            "id": log.id,
-            "type": log.get_type_display(),
-            "time": log.time.isoformat(),
-            "tag": str(log.tag),
-            "user_id": log.tag.owner.id,
-        })
+        data.append(
+            {
+                'id': log.id,
+                'type': log.get_type_display(),
+                'time': log.time.isoformat(),
+                'tag': str(log.tag),
+                'user_id': log.tag.owner.id,
+            }
+        )
 
-    return JsonResponse({"status": "success", "logs": data}, status=200)
-    
+    return JsonResponse({'status': 'success', 'logs': data}, status=200)
